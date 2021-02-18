406e542ee33efbdd3f92cf97dba2aa0c45d39f11
DaniloLima122 <limadanilo06@gmail.com>
2021-02-18 18:54:47 -0300

    finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeService } from './services/theme/theme.service';
 
 @Component({
